66a2a8c57b319e7d6c79620a285934f10d007124
Anton777-max <Anton-Samara.777@yandex.ru>
2025-04-23 12:04:14 +0300

    Update main.py

--- PythonProjects/Selenium Test/main.py
+++ PythonProjects/Selenium Test/main.py
@@ -40,6 +40,5 @@ print(message)'''
 '''response_change_name = requests.put(url = f'{URL}/pokemons', headers = HEADER, json = body_change_name)
 print(response_change_name.text)'''
 
-response_add_pokeball = requests.post(url = f'{URL}/trainers/add_pokeball', headers = HEADER, json = body_add_pokeball)
-print(response_add_pokeball.text)
+
 
