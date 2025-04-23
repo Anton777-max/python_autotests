adfab7a1332e90443b3bd93bfd743c4435b88d9e
Anton777-max <Anton-Samara.777@yandex.ru>
2025-04-23 12:02:39 +0300

    Update test_pokemons.py

--- PythonProjects/Selenium Test/tests/test_pokemons.py
+++ PythonProjects/Selenium Test/tests/test_pokemons.py
@@ -5,14 +5,19 @@ URL = 'https://api.pokemonbattle.ru/v2'
 TOKEN = '06e377f5174bdfab3be8a74d274f2162'
 HEADER = {'Content-Type': 'application/json', 'trainer_token': TOKEN}
 TRAINER_ID = '29110'
-TRAINER_NAME = 'Michael'
 
 def test_status_code():
-    response = requests.get(f'{URL}/trainers', params={'trainer_id': TRAINER_ID}, headers=HEADER)
+    response = requests.get(url= f'{URL}/pokemons', params={'trainer_id': TRAINER_ID})
     assert response.status_code == 200
 
-def test_trainer_name_in_response():
-    response = requests.get(f'{URL}/trainers', params={'trainer_id': TRAINER_ID}, headers=HEADER)
-    assert response.status_code == 200
+def test_part_of_response():
+    response_get = requests.get(url= f'{URL}/trainers', params={'trainer_id': TRAINER_ID})
+    assert response_get.json()["data"][0]["trainer_name"] == 'Michael'
+
+    @pytest.mark.parametrize('key, value', [('trainer_name', 'Michael'), ('id',f'{TRAINER_ID}')])
+    def test_parametrize(key,value):
+        response_parametrize = requests.get(url= f'{URL}/trainers', params={'trainer_id': TRAINER_ID})
+        assert response_parametrize.json()["data"][0][key] == value
+
 
 
